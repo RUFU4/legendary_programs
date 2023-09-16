f240f7647bfffc14d6d642da6d1d56457bf8339a
RUFU4 <81432116+RUFU4@users.noreply.github.com>
2023-09-17 00:17:13 +0300

    base.py

--- 2lab/base.py
+++ 2lab/base.py
@@ -1,20 +1,24 @@
+"""Написать функцию, которая принимает список, состоящий из n элементов, и возвращает их сумму"""
+
 x = []
 
 
-def summa(h):
-    p = 0
-    for k in range(0, len(h)):
-        p = p + h[k]
-    return p
+def task4():
+    def summa(h):
+        p = 0
+        for k in range(0, len(h)):
+            p = p + h[k]
+        return p
+
+    print('Введите числа для списка (Для окончания записи напишите букву k)')
+
+    while True:
+        u = input()
+        if u == 'k':
+            break
+        else:
+            x.append(int(u))
+    print(summa(x))
 
 
-print('Введите числа для списка (Для окончания записи напишите букву k)')
-
-while True:
-    u = input()
-    if u == 'k':
-        break
-    else:
-        x.append(int(u))
-
-print(summa(x))
+task4()
