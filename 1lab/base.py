7990ea26f811ba87ba248b8f277a0213c0a9ce94
RUFU4 <81432116+RUFU4@users.noreply.github.com>
2023-09-16 23:59:28 +0300

    base.py

--- 1lab/base.py
+++ 1lab/base.py
@@ -1,24 +1,27 @@
-h1 = []
-h2 = []
-x = []
-y = []
+''' Написать функцию, которая принимает два целочисленных списка, содержащих по три элемента каждый,
+и возвращает новый список, состоящий из двух элементов, являющихся средними во входящих списках.
+Например, для следующих входящих списков {1,2,3} и {3,4,5} итоговым будет {2,4}'''
 
 
-def inputik(x, y):
-    p = [x[1], y[1]]
-    return p
+def task4():
+    h1 = []
+    h2 = []
 
+    def middle_value(x, y):
+        p = [x[1], y[1]]
+        return p
 
-print('Введиите значения для двух списков по-очереди')
+    print('Введиите значения для двух списков по-очереди')
 
-for k in range(3):
-    u = input()
-    h1.append(u)
-    u = None
+    for k in range(7):
+        if k > 3:
+            u = input()
+            h2.append(u)
+        elif k < 3:
+            u = input()
+            h1.append(u)
 
-for k in range(3):
-    u = input()
-    h2.append(u)
-    u = None
+    print(middle_value(h1, h2))
+    return
 
-print(inputik(h1, h2))
+task4()
